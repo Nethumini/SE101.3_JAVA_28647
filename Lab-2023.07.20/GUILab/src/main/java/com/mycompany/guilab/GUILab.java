439e4797b7adb3662a1e464c1308321231f6c092
Tharinda Pathirana <135990391+Nethumini@users.noreply.github.com>
2023-07-20 23:35:57 +0530

    Add files via upload

---
+++ Lab-2023.07.20/GUILab/src/main/java/com/mycompany/guilab/GUILab.java
@@ -0,0 +1,11 @@
+package com.mycompany.guilab;
+public class GUILab
+{
+
+    public static void main(String[] args) 
+    {
+       labpractical l= new labpractical();
+       l.show();
+
+    }
+}
---
+++ Lab-2023.07.20/GUILab2/src/main/java/com/mycompany/guilab2/GUILab2.java
@@ -0,0 +1,10 @@
+package com.mycompany.guilab2;
+public class GUILab2 
+{
+
+    public static void main(String[] args) 
+    {
+        Counter c= new Counter();
+        c.show();
+    }
+}
